1acddeeba32f80b5d57f379c48399e25ec472f84
Lily Beauvilliers <lily.beauvilliers@gmail.com>
2018-01-17 12:22:59 +0000

    chessboard

---
+++ ChessBoard.js
@@ -0,0 +1,14 @@
+characters = ['#',' '];
+board = '';
+size = 12;
+
+for (i = 0; i < size; i++) {
+    line = '';
+
+    for (j = 0; j < size; j++) {
+        line += characters[(i + j) % 2]
+    }
+    board += (line + '\n');
+}
+
+console.log(board);
